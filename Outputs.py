06bcfd04a3b6f41568780743e23bdefb118a5d90
Jack Krause <jmkrause@iastate.edu>
2023-12-13 01:06:27 -0600

    files added

---
+++ Outputs.py
@@ -0,0 +1,10 @@
+from Work1 import x
+
+val = x(3)
+print(val)
+
+
+
+
+
+
--- Work1.py
+++ Work1.py
@@ -1,2 +1,17 @@
 import sympy as sym
+a = sym.var("a")
+
+
+def x(i):
+    if i == 1:
+        return 0.5
+
+    return x(i-1) + 0.5
+
+
+
+
+
+
+
 
