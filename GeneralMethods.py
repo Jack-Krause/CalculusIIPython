89ab85a760f5c902095937a33cdd82fca8950e54
Jack Krause <jmkrause@iastate.edu>
2023-12-14 00:18:37 -0600

    sequence added

--- GeneralMethods.py
+++ GeneralMethods.py
@@ -5,24 +5,29 @@ class Sol:
     def __init__(self, n, a):
         self.a = a
         self.n = n
+        self.sequence = []
 
     def x(self):
         if self.n == 0:
+            self.sequence.append(0.5)
             return 0.5
-        return Sol.g(self.n, self.a)
+        return self.g(self.n)
 
-    @staticmethod
-    def g(n, a):
+    def g(self, n):
         if n == 0:
             return 0.5
 
-        x_n = Sol.g(n-1, a)
-        return a * x_n * (1 - x_n)
+        x_previous = self.g(n - 1)
+        x_n = self.a * x_previous * (1 - x_previous)
+        self.sequence.append(format(x_n, '.4f'))
+        return x_n
 
     def fShow(self):
         print(Fraction.from_float(self.x()))
 
     def show(self):
         print(str(self.n) + ": " + format(self.x(), '.4f'))
+
+
 
 
--- Work1.py
+++ Work1.py
@@ -7,8 +7,9 @@ a = 3.5
 x1 = Sol(1, a)
 x2 = Sol(2, a)
 # x1 = Fraction.from_float(x1)
+#
+# for i in range(101):
+#     Sol(i, a).show()
+#     print("---")
 
-for i in range(101):
-    Sol(i, a).show()
-    print("---")
 
