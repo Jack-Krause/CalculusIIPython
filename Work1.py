6e67619bdcc4e71998f4a09616fa36a68be03d31
Jack Krause <jmkrause@iastate.edu>
2023-12-14 01:37:47 -0600

    output modified

--- Work1.py
+++ Work1.py
@@ -3,15 +3,15 @@ import sympy as sym
 from GeneralMethods import Sol
 from GeneralMethods import Sequence
 
-a = 3.5
+a = 3.49
 x_100 = Sol(100, a)
 x_100.x()
 print(x_100.sequence)
 seq_x = Sequence(x_100.sequence)
 print(seq_x.findOccurrences())
 
-for i in range(15):
-    a = round(a+0.1, 1)
+for i in range(11):
+    a = round(a+0.01, 2)
     a_x = Sol(100, a)
     a_x.x()
     print("--" + str(a) + "--")
