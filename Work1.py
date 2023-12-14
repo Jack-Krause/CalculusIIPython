0866250cd4533dce7dc2a604c0c74bcea66dbde1
Jack Krause <jmkrause@iastate.edu>
2023-12-13 22:58:27 -0600

    comm

--- Work1.py
+++ Work1.py
@@ -3,19 +3,12 @@ from fractions import Fraction
 import sympy as sym
 from GeneralMethods import Sol
 
-a = 2.5
+a = 3.5
 x1 = Sol(1, a)
 x2 = Sol(2, a)
 # x1 = Fraction.from_float(x1)
 
-x0 = Sol(0, a).show()
-x1.show()
-x2.show()
-x3 = Sol(3, a).show()
-x4 = Sol(4, a).show()
-x5 = Sol(5, a).show()
-x6 = Sol(6, a).show()
-x7 = Sol(7, a).show()
-x8 = Sol(8, a).show()
-x9 = Sol(9, a).show()
-x10 = Sol(10, a).show()
+for i in range(101):
+    Sol(i, a).show()
+    print("---")
+
